302aa5210665921a3620d0397856b35ab15a4bea
stdev0617 <stdev0617@gmail.com>
2020-06-23 23:17:02 +0900

    added ListFactory and Listlink class in abstract factory pattern

---
+++ src/abstractFactory/ListLink.java
@@ -0,0 +1,12 @@
+package abstractFactory;
+
+public class ListLink extends Link {
+    public ListLink(String caption, String url) {
+        super(caption, url);
+    }
+
+    @Override
+    public String makeHTML() {
+        return " <li><a href=\""+url+"\">" + caption + "</a></li>\n";
+    }
+}
