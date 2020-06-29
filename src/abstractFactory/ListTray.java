6d6c429a725ea35535cce467cd75bad6829608bb
stdev0617 <stdev0617@gmail.com>
2020-06-30 00:00:06 +0900

    added ListTray

---
+++ src/abstractFactory/ListTray.java
@@ -0,0 +1,25 @@
+package abstractFactory;
+
+import iterate.Iterator;
+
+public class ListTray extends Tray {
+    public ListTray(String caption) {
+        super(caption);
+    }
+
+    @Override
+    public String makeHTML() {
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("<li>\n");
+        buffer.append(caption + "\n");
+        buffer.append("<ul>\n");
+        Iterator it = tray.iterator();
+        while (it.hasNext()) {
+            Item item = (Item) it.next();
+            buffer.append(item.makeHTML());
+        }
+        buffer.append("</ul>");
+        buffer.append("</li>");
+        return buffer.toString();
+    }
+}
