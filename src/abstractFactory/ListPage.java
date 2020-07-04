00e44f7982c86dee3254f8ede6f870556cf12308
stdev0617 <stdev0617@gmail.com>
2020-07-04 23:26:57 +0900

    added ListPage in abstractFactory

---
+++ src/abstractFactory/ListPage.java
@@ -0,0 +1,28 @@
+package abstractFactory;
+import factory.*;
+import iterate.Iterator;
+
+public class ListPage extends Page {
+
+    public ListPage(String title, String author) {
+        super(title, author);
+    }
+
+    @Override
+    public String makeHTML() {
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("<html><head><title>"+title+"</title></head>\n");
+        buffer.append("<body>\n");
+        buffer.append("<h1>" + title + "</h1>\n");
+        buffer.append("<ul>\n");
+        Iterator it = (Iterator) content.iterator();
+        while (it.hasNext()) {
+            Item item = (Item)it.next();
+            buffer.append(item.makeHTML());
+        }
+        buffer.append("</ul>\n");
+        buffer.append("<hr><address>" + author + "</address>");
+        buffer.append("</body></html>\n");
+        return buffer.toString();
+    }
+}
